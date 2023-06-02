619641216fa464c2a24bafc523fd76c41866cbb3
Jobin Kadalickal <jobinkadalickal@hotmail.com>
2023-06-02 14:43:31 -0400

    Completed Create and Edit functionality for menuitem

--- SpiceProject/Areas/Admin/Controllers/MenuItemController.cs
+++ SpiceProject/Areas/Admin/Controllers/MenuItemController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpiceProject.Data;
+using SpiceProject.Models.ViewModels;
+using SpiceProject.Utility;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,12 +16,21 @@ namespace SpiceProject.Areas.Admin.Controllers
     public class MenuItemController : Controller
     {
         private readonly ApplicationDbContext _db;
-        private readonly IWebHostEnvironment _hostingEnvironment;   // because we are saving images on the server we have to you the hosting evnironment
+        private readonly IWebHostEnvironment _iWebHostEnvironment;   // because we are saving images on the server we have to you the hosting evnironment
 
-        public MenuItemController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        [BindProperty]  // if you see in the Categry controller, you can see that we pass in the Category multiple times to action methods, by binding we can directly use that property without having to pass it as argument. it is bascally saying that this contoller has this binding property attached to it 
+        public MenuItemViewModel MenuItemVM { get; set; } // NOTE how it is intialized in the constructor for use 
+
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment IWebHostEnvironment)
         {
             _db = db;
-            _hostingEnvironment = hostingEnvironment;
+            _iWebHostEnvironment = IWebHostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
 
         }
         public async Task<IActionResult> Index()
@@ -26,5 +38,149 @@ namespace SpiceProject.Areas.Admin.Controllers
             var menuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuItems);
         }
+
+        //GET -CREATE
+        public IActionResult Create()// didnt have to pass in MenuItem because we used binding above, see the difference comapared to the create method in the category controller
+        {
+            return View(MenuItemVM);
+        }
+
+
+        //POST - CREATE
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()   // note we dont need to call MenueItemViewModel in the parenthesis because we binded to it already 
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);  
+            await _db.SaveChangesAsync();
+
+            // Work on the image saving section
+
+            string webRootPath = _iWebHostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = await _db.MenuItem.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if(files.Count > 0)
+            {
+                // this means that file has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using(var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                // means that no file has been uploaded so use default
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+
+            }
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        //GET -EDIT
+        public async Task<IActionResult> Edit(int? id)// didnt have to pass in MenuItem because we used binding above, see the difference comapared to the create method in the category controller
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+            MenuItemVM.SubCategory = await _db.SubCategory.Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();    // this is just to load the first time 
+
+            if(MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+        }
+
+
+        //POST - EDIT
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)   // note we dont need to call MenueItemViewModel in the parenthesis because we binded to it already 
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());      //see ep 113
+
+            if (!ModelState.IsValid)
+            {
+                //if not valid, we need to populate the menue item to send back to the menuitem view model 
+                MenuItemVM.SubCategory = await _db.SubCategory.Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();
+                return View(MenuItemVM);
+            }
+
+    
+            // Work on the image saving section
+
+            string webRootPath = _iWebHostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = await _db.MenuItem.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if (files.Count > 0)
+            {
+                // this means that new image has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension_new = Path.GetExtension(files[0].FileName);
+
+                //Delete the orginal file
+                var imagePath = Path.Combine(webRootPath, menuItemFromDb.Image.TrimStart('\\'));
+
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                //we will upload the new file
+
+                using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_new), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension_new;    // this now saves to the database
+            }
+
+            //now if a user changes andthing we will push it to the database
+            menuItemFromDb.Name = MenuItemVM.MenuItem.Name;
+            menuItemFromDb.Description = MenuItemVM.MenuItem.Description;
+            menuItemFromDb.Price = MenuItemVM.MenuItem.Price;
+            menuItemFromDb.SpiceLevel = MenuItemVM.MenuItem.SpiceLevel;
+            menuItemFromDb.CategoryId = MenuItemVM.MenuItem.CategoryId;
+            menuItemFromDb.SubCategoryId = MenuItemVM.MenuItem.SubCategoryId;
+
+
+
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
     }
 }
---
+++ SpiceProject/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiceProject.Utility
+{
+    public static class SD  // contains all the constatns for our website
+    {
+        public const string DefaultFoodImage = "default_food.png";
+    }
+}
